1df6032c27ab205f9503a13ef5516dbb15aab458
ankit <ankit.cni99@gmail.com>
2023-08-28 23:00:16 +0530

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -20,9 +20,13 @@ router.get('/post',(req, res)=>{
     res.render('createpost')
 })
 
-// router.get('/new',(req, res)=>{
-//     res.render('createpost')
-// })
+router.get('/login',(req, res) => {
+	res.render('login')
+});
+
+router.get('/signup',(req , res) => {
+	res.render('signup')
+})
 
 var storage = multer.diskStorage({
 	destination: (req, file, cb) => {
